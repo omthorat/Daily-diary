6cd84639561446e2f1270ee12b15ece5ba892703
Omkar thorat <114384305+omthorat@users.noreply.github.com>
2024-04-30 10:45:30 +0000

    feat: server is set up

---
+++ backend/data/note.js
@@ -0,0 +1,19 @@
+const notes = [
+    {
+        _id: "1",
+        name: "omkar"
+    },
+    {
+        _id: "2",
+        name: "ketan"
+    },
+    {
+        _id: "3",
+        name: "guru"
+    },
+    {
+        _id: "4",
+        name: "omkar"
+    },
+]
+module.exports=notes
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,20 @@
 const express = require("express");
-const app = express();
+const note=require("./data/note")
+const dotenv= require("dotenv")
 
-app.listen(5000, console.log("port 500000"));
+const app = express();
+dotenv.config()
+
+app.get("/",(req,res)=>{
+    res.send("...api is running fine")
+})
+app.get("/api",(req,res)=>{
+    res.json(note)
+})
+app.get("/api/:id",(req,res)=>{
+    const name=note.find((n)=>n._id === req.params.id)
+    console.log(name)
+    res.send(name)
+})
+const Port= process.env.PORT || 5011
+app.listen(Port, console.log(`port ${Port} or 5011`));
