baf1dca2c564ee027dfd9e7cd5d28271f4866d07
omthorat <114384305+omthorat@users.noreply.github.com>
2024-05-14 23:37:29 +0530

    feat: added frontend

--- frontend/src/components/MyNotes/MyNotes.js
+++ frontend/src/components/MyNotes/MyNotes.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MyNotesScreen from "../screens/MyNotesScreen";
+import axios from "axios";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./MyNotes.css";
 const MyNotes = () => {
+  const [notes, setNotes] = useState([]);
+  const fetchApi = async () => {
+    const data = await axios.get("/api");
+    setNotes(data.data);
+    console.log(data.data);
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
     <>
       <MyNotesScreen title="Welcome back omkar thorat ...">
@@ -11,28 +22,32 @@ const MyNotes = () => {
           <Button variant="dark" size="lg" className="create-btn">
             Create New Item
           </Button>
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
-              </Card.Subtitle>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Card.Link>
-                <Link to="/edititem">
-                  <Button variant="primary">Edit</Button>
-                </Link>
-              </Card.Link>
-              <Card.Link>
-                <Link>
-                  <Button variant="danger">Delete</Button>
-                </Link>
-              </Card.Link>
-            </Card.Body>
-          </Card>
+          {notes.map((note) => {
+            return (
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>{note.name}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Card Subtitle
+                  </Card.Subtitle>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Card.Link>
+                    <Link to="/edititem">
+                      <Button variant="primary">Edit</Button>
+                    </Link>
+                  </Card.Link>
+                  <Card.Link>
+                    <Link>
+                      <Button variant="danger">Delete</Button>
+                    </Link>
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </div>
       </MyNotesScreen>
     </>
